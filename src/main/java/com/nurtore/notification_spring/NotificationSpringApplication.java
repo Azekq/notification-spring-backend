ba180839df62e957569c64bb176c390e6f258812
Nurtore <kelesovnurtore@gmail.com>
2025-03-22 16:24:43 +0500

    Initialize Spring Boot project with Gradle setup, including essential configuration files, application properties, and basic structure for a notification service. Added .gitignore and .gitattributes for environment-specific settings.

---
+++ src/main/java/com/nurtore/notification_spring/NotificationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nurtore.notification_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurtore/notification_spring/NotificationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurtore.notification_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
